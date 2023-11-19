3bac8a55720bd399b8ccc773007d4db37d49f935
Orloxx23 <madorlix@gmail.com>
2023-11-19 04:59:21 -0500

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,17 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { IconShoppingCart, IconShoppingCartCopy } from "@tabler/icons-react";
 import { useCart } from "@/context/CartContext";
 import { formatPrice } from "@/utils/functions";
 
 export default function Product({ item }) {
-  const { addToCart } = useCart();
-  const [added, setAdded] = React.useState(false);
+  // const { addToCart } = useCart();
+  const [added, setAdded] = useState(false);
 
   const add = () => {
     setAdded(true);
-    addToCart(item);
+    // addToCart(item);
     setTimeout(() => {
       setAdded(false);
     }, 2000);
@@ -22,12 +22,14 @@ export default function Product({ item }) {
       <section className="w-full px-10 md:px-40 py-10 md:py-48 flex flex-col justify-center items-center gap-10 min-h-0 ">
         <div className="columns-1 flex flex-wrap sm:flex-nowrap sm:gap-24	">
           <div className="columns-1 pb-5 sm:pb-0 sm:min-h-978px sm:w-6/12">
-            <img
-              src={item.image}
-              alt=""
-              className="w-full h-[70%] object-cover"
-              draggable="false"
-            />
+            {item && (
+              <img
+                src={item.image}
+                alt=""
+                className="w-full h-[70%] object-cover"
+                draggable="false"
+              />
+            )}
             <h2 className="text-1xl text-center md:text-2xl font-medium mt-0 sm:mt-4">
               Todo hecho por expertos, Dulce Tentación está hecho para tus
               momentos de placer
@@ -38,14 +40,14 @@ export default function Product({ item }) {
           </div>
           <div className="columns-1 min-h-978px sm:w-6/12">
             <h2 className="text-2xl md:text-5xl font-medium pb-4 sm:pb-4">
-              {item.name}
+              {item && item.name}
             </h2>
-            <p className="mb-5 opacity-50">{item.id}</p>
+            <p className="mb-5 opacity-50">{item && item.id}</p>
             <h1 className="text-red-500 text-xl md:text-4xl text-left pb-4 sm:pb-6">
-              {formatPrice(item.price)} COP
+              {item && formatPrice(item.price)} COP
             </h1>
             <p className="text-gray-700 text-xl font-medium text-left pb-5 sm:pb-7">
-              {item.description}
+              {item && item.description}
             </p>
             <button
               onClick={add}
