2aa03827b6e7feb1e002e74a084afa69c578bce2
Orloxx23 <madorlix@gmail.com>
2023-11-20 16:16:07 -0500

    Update page.jsx

--- src/app/catalogue/page.jsx
+++ src/app/catalogue/page.jsx
@@ -1,9 +1,13 @@
+"use client";
+
 import React from "react";
 import { db } from "@/firebase";
 import { collection, query, getDocs, where } from "firebase/firestore";
 import Product from "@/components/Product";
 
-export default async function page() {
+export default function page() {
+  const [products, setProducts] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   const getProduct = async () => {
     const q = query(collection(db, "products"), where("stock", ">", 0));
 
@@ -22,15 +26,24 @@ export default async function page() {
     return _products;
   };
 
-  let products = await getProduct();
+  React.useEffect(() => {
+    getProduct().then((data) => {
+      setProducts(data);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <div className="px-40 py-10">
       <h1 className="font-bold mb-5 sm:text-3xl">Catálogo</h1>
       <div className="grid grid-cols-4 gap-5">
-        {products.map((product) => (
-          <Product key={product.id} item={product} />
-        ))}
+        {loading ? (
+          <p>Espera...</p>
+        ) : products.length > 0 ? (
+          products.map((product) => <Product key={product.id} item={product} />)
+        ) : (
+          <p>No hay productos</p>
+        )}
       </div>
     </div>
   );
